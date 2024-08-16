fda616f7a94cbeba454d35802bf53bf84c5f0805
Matheus Alexandre <matheusasj@live.com>
2024-08-15 22:44:55 -0300

    build: modified simulation execution
    Number of generations was increased to 300, reflecting the original
    values.
    
    Added a visualization for the initial sim configuration.

--- GA-SIR/Program.cs
+++ GA-SIR/Program.cs
@@ -3,11 +3,13 @@
 var (susPerDay, infPerDay, recPerDay) = Utils.ReadFile(args[0]);
 
 // Setting up simulation variables
-int popSize = 20, bitSize = 10, numGenerations = 2, days = susPerDay.Length;
+int popSize = 20, bitSize = 10, numGenerations = 300, days = susPerDay.Length;
 float reprodutionRate = 0.3F, crossoverRate = 0.4F, mutationRate = 0.05F;
 uint totalNodes = susPerDay[0] + infPerDay[0] + recPerDay[0];
 
 // Instantiating the GA class and running the 
+Utils.PrintInitialConfig(args[0], popSize, numGenerations, bitSize, days, reprodutionRate, crossoverRate, mutationRate, totalNodes);
+
 var ag = new GeneticAlgorithm(popSize, totalNodes, infPerDay, recPerDay, new Random(), 7);
 
 ag.Run(reprodutionRate, crossoverRate, mutationRate, numGenerations, bitSize, days);
